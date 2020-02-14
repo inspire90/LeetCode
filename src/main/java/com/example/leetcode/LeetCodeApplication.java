79912611840e2ec9556c5ac277aaff6b7144d207
inspire90 <july2019@users.noreply.github.com>
2020-02-14 15:42:27 +0800

    Initial commit

---
+++ src/main/java/com/example/leetcode/LeetCodeApplication.java
@@ -0,0 +1,16 @@
+package com.example.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shentao
+ */
+@SpringBootApplication
+public class LeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeApplication.class, args);
+    }
+
+}
