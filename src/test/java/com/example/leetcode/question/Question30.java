bec999eef263469cd6bafb9a1c343e158927b54d
inspire90 <july2019@users.noreply.github.com>
2020-02-15 21:13:51 +0800

    commit

--- src/test/java/com/example/leetcode/question/Question30.java
+++ src/test/java/com/example/leetcode/question/Question30.java
@@ -1,5 +1,7 @@
 package com.example.leetcode.question;
 
+import com.sun.org.apache.xerces.internal.xinclude.XPointerElementHandler;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,11 +18,15 @@ import static java.util.stream.Collectors.toMap;
  */
 public class Question30 {
     public static void main(String[] args) {
-        List<Integer> list = findSubstring("ababaab", new String[]{"ab", "ba", "ba"});
+        long time = System.currentTimeMillis();
+        System.out.println("----------------------start-----------------");
+        List<Integer> list = findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake",
+                new String[]{"fooo", "barr", "wing", "ding", "wing"});
+        System.out.println("duration = " + (System.currentTimeMillis() - time) / 1000);
         list.forEach(System.out::println);
     }
 
-    public static List<Integer> findSubstring(String s, String[] words) {
+    /*public static List<Integer> findSubstring(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
         if ("".equals(s) || words.length == 0) {
             return result;
@@ -73,24 +79,106 @@ public class Question30 {
             }
         }
         return true;
-    }
+    }*/
 
     /*public static List<Integer> findSubstring(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
-
-        return null;
-    }
-
-    private static int isMatch(String s, String[] words, int i) {
-        if (s.indexOf(words[i]) == -1) {
-            return -1;
+        if ("".equals(s) || words.length == 0) {
+            return result;
         }
-        String[] split = s.split(words[i]);
-        for (int j = 0; j < split.length; j++) {
-            if (isMatch(split[j], words, j) == -1) {
-                return -1;
+        int step = words[0].length();
+        int len = step * words.length;
+        for (int i = 0; i <= s.length() - len; i++) {
+            String temp = s.substring(i, i + len);
+            Map<Integer, Integer> map = new HashMap<>(words.length);
+            for (int count = 0; count < words.length; count++) {
+                int begin = count * step;
+                String stepStr = temp.substring(begin, begin + step);
+                for (int j = 0; j < words.length; j++) {
+                    String word = words[j];
+                    if (stepStr.equals(word) && !map.containsKey(begin) && !map.containsValue(j)) {
+                        map.put(begin, j);
+                    }
+                }
+                if (map.entrySet().size() == words.length) {
+                    result.add(i);
+                }
             }
         }
-        return i;
+        return result;
     }*/
+
+    /*public static List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if ("".equals(s) || words.length == 0) {
+            return result;
+        }
+        int step = words[0].length();
+        int len = step * words.length;
+        for (int i = 0; i <= s.length() - len; i++) {
+            String temp = s.substring(i, i + len);
+            Map<String, Integer> map = new LinkedHashMap<>(words.length);
+            int position = 0;
+            for (int count = 0; count < words.length; count++) {
+                int index = temp.indexOf(words[count], position);
+                if (index == -1 || index % step != 0) {
+                    break;
+                }
+                if (map.containsKey(words[count])) {
+                    position = index + step;
+                    index = temp.indexOf(words[count], position);
+                    if (index == -1 || index % step != 0) {
+                        break;
+                    }
+
+                }
+                map.put(words[count], index);
+                if (map.keySet().size() == words.length) {
+                    result.add(i);
+                }
+            }
+        }
+        return result;
+    }*/
+
+    public static List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if ("".equals(s) || words.length == 0) {
+            return result;
+        }
+        int step = words[0].length();
+        int len = step * words.length;
+        Map<String, Integer> countMap = new HashMap<>();
+        for (String word : words) {
+            if (countMap.containsKey(word)) {
+                int count = countMap.get(word);
+                count++;
+                countMap.put(word, count);
+            } else {
+                countMap.put(word, 1);
+            }
+        }
+        for (int i = 0; i <= s.length() - len; i++) {
+            String word = s.substring(i, i + len);
+            for (int j = 0; j < words.length; j++) {
+                String temp = word.substring(j, j + step);
+                if (countMap.containsKey(temp)) {
+                    int count = countMap.get(temp);
+                    if (count == 1) {
+                        countMap.remove(temp);
+                    } else {
+                        count--;
+                        countMap.put(temp, count);
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (countMap.keySet().size() == 0) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
 }
